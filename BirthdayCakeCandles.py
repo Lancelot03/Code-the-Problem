7f418668e8c00d1f163ef63bcdd1882d72a0e42d
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-03-29 23:58:35 +0530

    Create BirthdayCakeCandles.py

---
+++ BirthdayCakeCandles.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(candles):
+    return candles.count(max(candles))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
