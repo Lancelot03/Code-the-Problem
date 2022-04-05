c8be1e4d32e3050edeef3135b60758e8a6567560
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-04-05 23:17:43 +0530

    Create A Very Big Sum.py

---
+++ A Very Big Sum.py
@@ -0,0 +1,19 @@
+import os
+import random
+import re
+import sys
+def aVeryBigSum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
