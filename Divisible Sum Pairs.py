088452f3c9b42f94a57efe8fc74a997d31ecce2b
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-04-09 23:02:32 +0530

    Create Divisible Sum Pairs.py

---
+++ Divisible Sum Pairs.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(n-1):
+        j = i+1
+        while j < n:
+            if ((ar[i] + ar[j]) % k) == 0:
+                count += 1
+            j += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
