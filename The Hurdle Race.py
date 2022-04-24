bacb7672dd2b375dfe66b1181bc6da336998dd7e
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-04-24 23:18:27 +0530

    Create The Hurdle Race.py

---
+++ The Hurdle Race.py
@@ -0,0 +1,25 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+def hurdleRace(k, height):
+    return max(max(height)-k,0)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
