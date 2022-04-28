8b190efb967fab7e7d9cc459c4e06701ac127de5
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-04-28 22:38:34 +0530

    Create Angry Professor.py

---
+++ Angry Professor.py
@@ -0,0 +1,28 @@
+import math
+import os
+import random
+import re
+import sys
+
+def angryProfessor(k, a):
+    return 'YES' if sum(map(lambda x: x <= 0, a)) < k else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        a = list(map(int, input().rstrip().split()))
+
+        result = angryProfessor(k, a)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
