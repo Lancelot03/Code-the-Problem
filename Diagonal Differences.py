8d98e27d10404564b87380706eefc08f6b8e168f
Harsh <90236987+Lancelot03@users.noreply.github.com>
2022-06-19 02:42:39 +0530

    Create Diagonal Differences.py

---
+++ Diagonal Differences.py
@@ -0,0 +1,31 @@
+import math
+import os
+import random
+import re
+import sys
+def diagonalDifference(arr):
+    n = len(arr)
+    left = 0
+    right = 0
+
+    for i in range(n):
+        left += arr[i][i]
+        right += arr[i][n-i-1]
+
+    return abs(left - right)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
