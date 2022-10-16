2b6d64d38cdcc7f0f005aceef047b7688d7c1897
Shubham Kumar Singh <81859951+Shubh-k04@users.noreply.github.com>
2022-10-17 00:10:42 +0530

    Create MergeSort.py

---
+++ MergeSort.py
@@ -0,0 +1,48 @@
+
+def mergeSort(arr):
+	if len(arr) > 1:
+
+		# Finding the mid of the array
+		mid = len(arr)//2
+
+		# Dividing the array elements
+		L = arr[:mid]
+
+		# into 2 halves
+		R = arr[mid:]
+
+		# Sorting the first half
+		mergeSort(L)
+
+		# Sorting the second half
+		mergeSort(R)
+
+		i = j = k = 0
+
+		# Copy data to temp arrays L[] and R[]
+		while i < len(L) and j < len(R):
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+
+		# Checking if any element was left
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+
+
+
+
+a = [2,5,3,6,1]
+mergeSort(a)
+print(*a)
