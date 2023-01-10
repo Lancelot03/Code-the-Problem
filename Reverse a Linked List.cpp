e84acee4f68fd63776c8652e90394a23ae464d38
Harsh <90236987+Lancelot03@users.noreply.github.com>
2023-01-11 00:45:52 +0530

    Update and rename 1234 to Reverse a Linked List.cpp

---
+++ Reverse a Linked List.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    ListNode* reverseList(ListNode* head) {
+
+       //step 1
+        ListNode* prev_p = NULL;
+        ListNode* current_p = head;
+        ListNode* next_p;
+       
+       //step 2
+        while(current_p) {
+
+            next_p = current_p->next;
+            current_p->next = prev_p;
+            
+            prev_p = current_p;
+            current_p = next_p;
+        }
+
+        head = prev_p; //step 3
+        return head;
+    }
+};
